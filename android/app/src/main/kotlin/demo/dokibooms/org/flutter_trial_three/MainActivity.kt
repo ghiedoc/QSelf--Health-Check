a57d24a0ca7156b6b20d48912bcd1370ecae47c0
ghiedoc <ghiedoc@gmail.com>
2020-12-23 00:41:58 +0800

    first commit get started module

---
+++ android/app/src/main/kotlin/demo/dokibooms/org/flutter_trial_three/MainActivity.kt
@@ -0,0 +1,6 @@
+package demo.dokibooms.org.flutter_trial_three
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: "OpenSans",
+        scaffoldBackgroundColor: const Color(0xFFFA8072),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 90),
+          child: Column(
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Get Started. \nYour Health \nMatters.",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36
+                  ),),
+                  SizedBox(height: 20,),
+                  Text("an application that helps you self-diagnose for 14-days",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16
+                  ),),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 30
+                ),
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/qselflogodark.png')
+                  )
+                )
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {},
+                      color: Color(0xFBB1F1F1F),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    child: Text("Get Started", style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ))
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    decoration: BoxDecoration(
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.white,
+                          ),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Text("Sign in", style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ))
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
