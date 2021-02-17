ac203893dd2d3df380fa82d653c5a60720597e4b
Chupols0920 <joshualagan099@gmail.com>
2021-02-17 21:23:38 +0800

    backbtn override

--- lib/screen/admin_dashboard.dart
+++ lib/screen/admin_dashboard.dart
@@ -5,6 +5,7 @@ import 'package:flutter_trial_three/screen/admin_userlist.dart';
 import 'package:flutter_trial_three/admin_contact/admin_user_contact.dart';
 import 'package:flutter_trial_three/admin_user_info/admin_info.dart';
 import 'login.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AdminDashboardPage extends StatefulWidget {
   static const routeName = '/adminDashboard';
@@ -59,9 +60,23 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
+    Future<bool> btnbackdd() async {
+      DateTime currentTime = DateTime.now();
+      bool backbtn = backButtonPressTime == null ||
+          currentTime.difference(backButtonPressTime) > Duration(seconds: 3);
+      if(backbtn){
+        backButtonPressTime = currentTime;
+        Fluttertoast.showToast(
+          msg: 'Double Tap to Close app',
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+        );
+        return false;
+      }
+      return true;
+    }
 
     final border = RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10.0),
@@ -106,163 +121,166 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
           ],
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: ListView(
-          children: <Widget>[
-            Text(
-              'Here you can view information about the users.',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.black54,
+      body: WillPopScope(
+        onWillPop: btnbackdd,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: <Widget>[
+              Text(
+                'Here you can view information about the users.',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.black54,
+                ),
               ),
-            ),
-            Container(
-              height: 130,
-              child: Padding(
-                padding: padding,
-                child: Card(
-                  shape: border,
-                  elevation: 3.0,
+              Container(
+                height: 130,
+                child: Padding(
+                  padding: padding,
+                  child: Card(
+                    shape: border,
+                    elevation: 3.0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.face_rounded, size: 50, color: Color(0xFFFF5555)),
+                          Text(
+                            'Hi, Administrator',
+                            style: TextStyle(
+                              fontSize: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //USER LIST
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return AdminUserListPage();
+                      },
+                    ),
+                  );
+                },
+                child: Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20.0, horizontal: 20.0),
-                    child: Row(
-                      children: [
-                        Icon(Icons.face_rounded, size: 50, color: Color(0xFFFF5555)),
-                        Text(
-                          'Hi, Administrator',
-                          style: TextStyle(
-                            fontSize: 32,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            //USER LIST
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AdminUserListPage();
-                    },
-                  ),
-                );
-              },
-              child: Expanded(
-                child: Padding(
-                  padding: padding,
-                  child: Card(
-                    shape: border,
-                    elevation: 3.0,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 10.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.wb_sunny, size: 50, color: Color(0xFFffc75f),),
-                              Text(
-                                'User Diagnosis Result',
-                                style: TextStyle(
-                                  fontSize: 30,
+                    padding: padding,
+                    child: Card(
+                      shape: border,
+                      elevation: 3.0,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 10.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.wb_sunny, size: 50, color: Color(0xFFffc75f),),
+                                Text(
+                                  'User Diagnosis Result',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AdminUserContact();
-                    },
-                  ),
-                );
-              },
-              child: Expanded(
-                child: Padding(
-                  padding: padding,
-                  child: Card(
-                    shape: border,
-                    elevation: 3.0,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 10.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.contact_page, size: 50, color: Color(0xFF1687a7)),
-                              Text(
-                                'User Contact',
-                                style: TextStyle(
-                                  fontSize: 30,
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return AdminUserContact();
+                      },
+                    ),
+                  );
+                },
+                child: Expanded(
+                  child: Padding(
+                    padding: padding,
+                    child: Card(
+                      shape: border,
+                      elevation: 3.0,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 10.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.contact_page, size: 50, color: Color(0xFF1687a7)),
+                                Text(
+                                  'User Contact',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return adminInfo();
-                    },
-                  ),
-                );
-              },
-              child: Expanded(
-                child: Padding(
-                  padding: padding,
-                  child: Card(
-                    shape: border,
-                    elevation: 3.0,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 10.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.flight, size: 50, color: Color(0xFF4AA786)),
-                              Text(
-                                'User Travel Information',
-                                style: TextStyle(
-                                  fontSize: 27,
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return adminInfo();
+                      },
+                    ),
+                  );
+                },
+                child: Expanded(
+                  child: Padding(
+                    padding: padding,
+                    child: Card(
+                      shape: border,
+                      elevation: 3.0,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 10.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.flight, size: 50, color: Color(0xFF4AA786)),
+                                Text(
+                                  'User Travel Information',
+                                  style: TextStyle(
+                                    fontSize: 27,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
